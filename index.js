de3af9f799455a63cfeaf6e0a3e1cf489e39238a
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-06-04 21:31:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+
+var number;
+function add(a, b){ 
+  number = a + b;
+  return number;}
+  
+  function subtract(a, b){
+    number= a-b;
+    return number;
+  }
+  
+  function multiply(a, b){
+    number=a*b;
+    return number;
+  }
+  
+  function divide(a,b){
+  number=a/b;
+  return number;}
+  
+  function increment(a){
+  number = a + 1;
+  return number;
+}
+ 
+ function decrement(a){
+   number = a - 1;
+   return number;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
